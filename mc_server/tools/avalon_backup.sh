5c5a8df0d230d3edd2cb4dbdf5a1c39c7d2d1c33
Eric Wright <eric@rdctd.com>
2011-09-15 13:21:44 -0400

    Correct savestate path

--- mc_server/tools/avalon_backup.sh
+++ mc_server/tools/avalon_backup.sh
@@ -2,6 +2,7 @@
 map_name=avalon
 map_path=/home/minecraft/nirrius-minecraft/mc_server/maps/$map_name
 backup_path=/home/minecraft/nirrius-minecraft/mc_server/backups/$map_name/`date "+%m-%d-%H-%M-%S"`.zip
+savestate_path=/home/minecraft/nirrius-minecraft/mc_server/savestates/$map_name.zip
 
 mkdir -p /home/minecraft/nirrius-minecraft/mc_server/backups/$map_name
 savestate_path=/home/minecraft/savestate/${map_name}_savestate.zip
