2c350e3393328ded2da54a125fb47ed6a1ba5e06
Eric Wright <eric@rdctd.com>
2011-09-15 13:16:27 -0400

    Correct backup path.

--- mc_server/tools/avalon_backup.sh
+++ mc_server/tools/avalon_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 map_name=avalon
-map_path=/home/minecraft/mc_server/maps/$map_name
-backup_path=/home/minecraft/mc_server/backups/$map_name/`date "+%m-%d-%H-%M-%S"`.zip
+map_path=/home/minecraft/nirrius-minecraft/mc_server/maps/$map_name
+backup_path=/home/minecraft/nirrius-minecraft/mc_server/backups/$map_name/`date "+%m-%d-%H-%M-%S"`.zip
 
 mkdir -p /home/minecraft/backups/$map_name
 savestate_path=/home/minecraft/savestate/${map_name}_savestate.zip
