ab5f4dd0db8701bdc37425086e5b45d7551211d5
Eric Wright <eric@rdctd.com>
2011-09-15 13:17:38 -0400

    Correct backup path.

--- mc_server/tools/shangri-la_backup.sh
+++ mc_server/tools/shangri-la_backup.sh
@@ -3,7 +3,7 @@ map_name=shangri-la
 map_path=/home/minecraft/nirrius-minecraft/mc_server/maps/$map_name
 backup_path=/home/minecraft/nirrius-minecraft/mc_server/backups/$map_name/`date "+%m-%d-%H-%M-%S"`.zip
 
-mkdir -p /home/minecraft/backups/$map_name
+mkdir -p /home/minecraft/nirrius-minecraft/mc_server/backups/$map_name
 savestate_path=/home/minecraft/savestate/${map_name}_savestate.zip
 cd $map_path; zip -r $backup_path *
 cp $backup_name $savestate_path
