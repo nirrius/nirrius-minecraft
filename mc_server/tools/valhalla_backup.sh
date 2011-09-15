1cb4a9804d21ef61c314cb28fe37194846991ca2
Eric Wright <eric@rdctd.com>
2011-09-15 13:49:31 -0400

    Correct backup tools.

--- mc_server/tools/valhalla_backup.sh
+++ mc_server/tools/valhalla_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 map_name=valhalla
 map_path=/home/minecraft/nirrius-minecraft/mc_server/maps/$map_name
-backup_path=/home/minecraft/nirrius-minecraft/mc_server/backups/$map_name/`date "+%m-%d-%H-%M-%S"`.zip
-savestate_path=/home/minecraft/nirrius-minecraft/mc_server/savestates/$map_name.zip
+backup_path=/home/minecraft/nirrius-minecraft/mc_server/backups/$map_name
+backup_name=$backup_path/`date "+%m-%d-%H-%M-%S"`.zip
+savestate_path=/home/minecraft/nirrius-minecraft/mc_server/savestates
 
 mkdir -p /home/minecraft/nirrius-minecraft/mc_server/backups/$map_name
-savestate_path=/home/minecraft/savestate/${map_name}_savestate.zip
-cd $map_path; zip -r $backup_path *
-cp $backup_name $savestate_path
+cd $map_path; zip -r $backup_name *
+cp $backup_name $savestate_path/${map_name}_savestate.zip
