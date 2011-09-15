3e0ef6143e6c217a116b0e52c67e7fd68cac4955
Eric Wright <eric@rdctd.com>
2011-09-15 12:57:56 -0400

    Rename bukkit to mc_server and correct paths.

--- mc_server/tools/restore_ramdisk_savestate.sh
+++ mc_server/tools/restore_ramdisk_savestate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # The map must be restored after reboot and the ramdisk must be writable.
 map_name=valhalla
-map_path=/home/minecraft/maps/$map_name
-savestate_path=/home/minecraft/bukkit/savestate
+map_path=/home/minecraft/mc_server/maps/$map_name
+savestate_path=/home/minecraft/bukkit/savestates
 cd $map_path
 unzip $savestate_path/ramdisk_savestate.zip
