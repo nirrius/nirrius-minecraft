7c01a75ec0697007e67709a377e14b6747528f5e
Minecraft <minecraft@genesis.nirrius.com>
2011-10-01 15:38:17 -0400

    Correct backup paths, gitignore file and whitelist.

--- mc-server/tools/make_ramdisk_savestate.sh
+++ mc-server/tools/make_ramdisk_savestate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Valhalla is on a ramdisk so the map must be backed up for restarts.
 map_name=$1
-map_path=/home/minecraft/mc-server/maps/$map_name
+map_path=/home/minecraft/nirrius-minecraft/mc-server/maps/$map_name
 savestate_path=/home/minecraft/mc-server/savestates
 mkdir -p $savestate_path
 cd $map_path
--- mc-server/tools/restore_ramdisk_savestate.sh
+++ mc-server/tools/restore_ramdisk_savestate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # The map must be restored after reboot and the ramdisk must be writable.
 map_name=$1
-map_path=/home/minecraft/mc-server/maps/$map_name
-savestate_path=/home/minecraft/bukkit/savestates
+map_path=/home/minecraft/nirrius-minecraft/mc-server/maps/$map_name
+savestate_path=/home/minecraft/nirrius-minecraft/savestates
 cd $map_path
 unzip $savestate_path/ramdisk_savestate.zip
