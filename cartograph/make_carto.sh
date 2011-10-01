814c231b0a4f985fd99dc5faf5b75c7247f94050
Minecraft <minecraft@genesis.nirrius.com>
2011-10-01 16:58:58 -0400

    Update cartograph path.

--- cartograph/make_carto.sh
+++ cartograph/make_carto.sh
@@ -3,10 +3,9 @@
 # Declarations
 map_name=$1
 carto_path=/home/minecraft/nirrius-minecraft/cartograph
-scp=www-developer@nirri.us
 map_path=$carto_path/../mc-server/maps/$map_name
 
-image_path=/home/www-server/redacted/site/public/resources/images/cartographs/$map_name
+image_path=/home/minecraft/nirrius-minecraft/site/resources/cartographs/$map_name
 
 # Render oblique and angled cartographs with a ~1000 block perimeter
 $carto_path/c10t/c10t -w $map_path -o /tmp/oblique_render.png -q -L -85,85,-85,85
@@ -18,9 +17,8 @@ convert -quality 0 +dither -colors 256 /tmp/oblique_render.png /tmp/oblique.png
 
 convert -quality 0 +dither -colors 256 /tmp/angled_render.png /tmp/angled.png
 
-# The user must have sudoers access to connect via SSH without a password
-scp /tmp/oblique.png $scp:$image_path/oblique.png
-scp /tmp/angled.png $scp:$image_path/angled.png
+cp /tmp/oblique.png $image_path/oblique.png
+cp /tmp/angled.png $image_path/angled.png
 
 # Clean up
 rm /tmp/oblique_render.png
@@ -28,3 +26,4 @@ rm /tmp/angled_render.png
 
 rm /tmp/oblique.png
 rm /tmp/angled.png
+
