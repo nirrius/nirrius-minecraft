ebac3492a12991d4ad851f79c19b73bb0eca4b89
Eric Wright <eric@rdctd.com>
2011-10-14 00:43:08 -0400

    Update paths, banned players, craftbukkit and delete whitelist symlink.

--- mc-server/vanilla.sh
+++ mc-server/vanilla.sh
@@ -2,4 +2,5 @@
 minecraft_path=/home/minecraft/nirrius-minecraft/mc-server
 screen_name=nirrius_mc
 
-cd $minecraft_path; screen -S $screen_name bash -c 'java -Xmx6000M -Xms3000M -server -jar minecraft_server.jar nogui'
+#cd $minecraft_path;
+screen -S $screen_name bash -c 'java -Xmx6000M -Xms3000M -server -jar minecraft_server.jar nogui'
