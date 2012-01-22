75000cf64884807bc78b22b73cb73236fcf35450
Eric Wright <eric@maetel>
2012-01-22 01:08:05 -0500

    Correct move issues.

--- mc-server/tools/restore_ramdisk_savestate.sh
+++ mc-server/tools/restore_ramdisk_savestate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # The map must be restored after reboot and the ramdisk must be writable.
 map_name=$1
-map_path=/home/minecraft/nirrius-minecraft/mc-server/maps/$map_name
-savestate_path=/home/minecraft/nirrius-minecraft/savestates
+map_path=/home/minecraft/nirrius-minecraft/mc-server/maps/
+savestate_path=/home/minecraft/nirrius-minecraft/mc-server/savestates
 cd $map_path
-unzip $savestate_path/ramdisk_savestate.zip
+unzip $savestate_path/${map_name}_savestate.zip
