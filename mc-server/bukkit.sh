cb337963440b5ec3ae78d13b9b6ae3f6fbf686f8
Eric Wright <eric@maetel>
2012-02-11 17:53:58 -0500

    General update

--- mc-server/bukkit.sh
+++ mc-server/bukkit.sh
@@ -2,4 +2,5 @@
 minecraft_path=/home/minecraft/nirrius-minecraft/mc-server
 screen_name=nirrius_mc
 
-cd $minecraft_path; screen -S $screen_name bash -c 'java -Xmx3100M -Xms2000M -server -jar craftBukkit.jar nogui'
+cd $minecraft_path; screen -S $screen_name bash -c 'java -Xmx3000M -Xms2000M -server -jar craftBukkit.jar nogui'
+
